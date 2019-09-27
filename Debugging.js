9142fa3baebeced43e5450286081410bd136198b
breaker2l <34549956+breaker2l@users.noreply.github.com>
2019-09-27 05:25:26 -0700

    Create Debugging.js

---
+++ Debugging.js
@@ -0,0 +1,19 @@
+var http = require("http")
+
+var s = http.createServer(function (req,res){
+    var body = 'Thanks for calling!\n';
+    var content_length = body.lengtth;
+    res.writeHead(200, {
+        'Content-Length': content_length,
+         'Content-Type': 'text/plain'
+    });
+    res.end(body);
+});
+
+s.listen(8080);
+
+#running the program
+node debugging.js
+
+#debugger
+node debug debugging.js
