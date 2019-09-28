ae59c4c451955c728e516554173b1a3edfedcc89
breaker2l <34549956+breaker2l@users.noreply.github.com>
2019-09-27 23:49:45 -0700

    Update simple_server.js

--- simple_server.js
+++ simple_server.js
@@ -8,3 +8,19 @@ function handle_incoming_request(req, res){
 
 var s = http.createServer(handle_incoming_request);
 s.listen(8080);
+
+
+<!--//failure response will look like this:
+{error: "missing data",
+ message: "You nust include a last name for the user" }
+
+//success responses will usually have a "data" object
+{error:null,
+ data: {
+     user: {
+         first_name: "Horatio",
+         laat_name: "Gadsplatt III"
+         email: "horatio@example.org"
+     }
+ }
+}-->
